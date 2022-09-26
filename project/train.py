3ae9a550dddf7fdcace79ddf9e99e0759e770b92
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2022-09-26 17:15:46 -0300

    :tada: add function in train.py

--- project/train.py
+++ project/train.py
@@ -1,5 +1,10 @@
-def say_hello(name: str): -> str
+def say_hello(name: str):
     print(f"Hello {name}, the Data Scientist")
 
 
+def greetings(name: str):
+    print(f"This is a greeting from {name}")
+
+
 say_hello('Nicolas')
+greetings("the Big Brother")
